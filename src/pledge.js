672f72063eeca1826405cf017e72a1482c241486
Alec Davidson <davidson9076@gmail.com>
2017-11-15 16:21:01 -0600

    up to chapter 4

--- src/pledge.js
+++ src/pledge.js
@@ -4,34 +4,89 @@ Promises Workshop: build the pledge.js ES6-style promise library
 ----------------------------------------------------------------*/
 // YOUR CODE HERE:
 
-function $Promise(executor){
+function $Promise(executor) {
 
-  if(typeof executor !== 'function') {
+  if (typeof executor !== 'function') {
     throw TypeError('executor function is not defined');
   }
   this._state = 'pending'
   this._assigned = false;
   this._value = undefined;
+  this._handlerGroups = []
 
   executor(this._internalResolve.bind(this), this._internalReject.bind(this))
 }
-//this._internalResolve
-$Promise.prototype._internalResolve  = function(someData){
-  console.log('this', this)
-  if (!$Promise.prototype._value && !this._assigned){
+
+$Promise.prototype._internalResolve = function (someData) {
+  if (!$Promise.prototype._value && !this._assigned) {
     this._value = someData;
     this._assigned = true;
     this._state = 'fulfilled'
+    this._callHandlers()
   }
 
 }
-$Promise.prototype._internalReject  = function(reason){
-  console.log('thisRej', this)
-  if (!this._value && !this._assigned){
+$Promise.prototype._internalReject = function (reason) {
+  if (!this._value && !this._assigned) {
     this._state = 'rejected'
     this._value = reason;
     this._assigned = true;
+    this._callHandlers()
   }
+}
+$Promise.prototype.catch = function (errorHandler) {
+  this.then(null, errorHandler)
+  return new $Promise(function(){
+
+  });
+}
+
+$Promise.prototype.then = function (resolveFunc, errorFunc) {
+  if (typeof resolveFunc !== 'function') {
+    resolveFunc = null;
+  }
+  if (typeof errorFunc !== 'function') {
+    errorFunc = null;
+  }
+  let newProm = new $Promise(function () {
+  })
+  this._handlerGroups.push({
+    successCb: resolveFunc,
+    errorCb: errorFunc,
+    downstreamPromise: newProm
+  })
+  this._callHandlers()
+  return newProm;
+}
+
+$Promise.prototype._callHandlers = function () {
+  if (this._state === 'fulfilled') {
+    this._handlerGroups.forEach((elem) => {
+      if (elem.called === undefined){
+        elem.successCb(this._value)
+        elem.called = true;
+      }
+
+    })
+  }
+  if (this._state === 'rejected') {
+    this._handlerGroups.forEach((elem) => {
+
+
+      if (elem.called === undefined){
+        if (typeof elem.errorCb === 'function'){
+          elem.errorCb(this._value)
+          elem.called = true;
+        }
+      }
+
+    })
+  }
+  let newGroups = this._handlerGroups.filter((elem) => {
+    return elem.called === undefined;
+  })
+  this._handlerGroups = newGroups;
+
 }
 
 
